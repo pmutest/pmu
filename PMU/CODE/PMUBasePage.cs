7ff70f7fd05dd3e6e43774f68d1a438085f47aa9
nishant kumar <profilemeup@outlook.com>
2013-06-22 17:22:29 +0530

    Files Added
    Added Service folder and Solution

---
+++ PMU/CODE/PMUBasePage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMU.CODE
+{
+    public class PMUBasePage : System.Web.UI.Page
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnPreInit(EventArgs e)
+        {
+            base.OnPreInit(e);
+        }
+
+        /// <summary>
+        /// Name: Page_Load
+        /// Purpose: Custom Page load for the base functions check the login status used 
+        ///          to check whether the user is logged in or not for accessing pages which require login. 
+        /// </summary>
+        protected void Page_Load(object sender, System.EventArgs e)
+        {
+            string pageURL = Request.Url.AbsoluteUri;
+            // If the Session value for the user is not set, then check the Cookie value for user's Validity.
+            //if (this.Session["Username"] == null)
+            //{
+            //    GlobalSessionCheck oSessionCheck = new GlobalSessionCheck();
+            //    if (!oSessionCheck.checkSession())
+            //    {
+            //        // Not a avalid User.
+            //        Server.Transfer("Login.aspx");
+            //        return;
+            //    }
+            //}
+        }
+    }
+}
---
+++ PMU/Home.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PMU.CODE.BLogic;
+using PMU.PMUService;
+
+namespace PMU
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                ResetSignUptxtFieldValue();
+            }
+
+        }
+
+        protected void btnSignIn_Click(object sender, EventArgs e)
+        {
+            string userName = txtSignInUserName.Value.Trim();
+            string password = txtSignInPassword.Value;
+            string validateInput = string.Empty;
+            bool IsUserExist = false;
+
+            validateInput = LoginDetail.validateSignInData(userName, password);
+            
+
+            if (validateInput == string.Empty)
+            {
+                IsUserExist = LoginDetail.IsAuthorizedUser(userName, password);
+            }
+        }
+
+        protected void btnSignUp_Click(object sender, EventArgs e)
+        {
+            string userName = txtUserName.Value.Trim();
+            string emailAdd = txtEmailAdd.Value.Trim();
+            string password = txtSignUpPassword.Value;
+            string confPassword = txtConfPassword.Value;
+            WSInteger isExist = new WSInteger();
+            isExist.IntegerValue = 0;
+
+            string validateInput = UserSignUpInfo.validateSignUpInputData(userName, emailAdd, password, confPassword);
+            User userInfo = new User();
+
+            isExist = UserSignUpInfo.IsUserExist(userName);
+
+            // Temprory testing condition
+            if (isExist.IntegerValue == 1)
+            {
+                userNameErrMsg.Text = "Username Allready exist";
+            }
+
+            if (validateInput == string.Empty && isExist.IntegerValue == 0)
+            {
+                try
+                {
+                    userInfo = UserSignUpInfo.InsertSignUpData(userName, emailAdd, password);
+                }
+                catch (Exception ee)
+                {
+
+                }
+            }
+        }
+
+        private void ResetSignUptxtFieldValue()
+        {
+            txtSignInUserName.Value = string.Empty;
+            txtEmailAdd.Value = string.Empty;
+            txtSignUpPassword.Value = string.Empty;
+            txtConfPassword.Value = string.Empty;
+            txtUserName.Value = string.Empty;
+        }
+    }
+}
